1b96ec69e72234cb198ef556be93522fce1d143e
Hafid Ardiansyah <62227964+hafidardiansyah@users.noreply.github.com>
2021-03-15 10:00:19 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,12 @@ class MyApp extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              SizedBox(height: 24),
+              SizedBox(height: 52),
               Image.asset(
                 'images/Illustration.png',
                 height: 300,
               ),
-              SizedBox(height: 24),
+              SizedBox(height: 52),
               Text("We’re Launching Soon!",
                   style: GoogleFonts.poppins(
                       fontSize: 64, fontWeight: FontWeight.w500)),
@@ -61,6 +61,7 @@ class MyApp extends StatelessWidget {
                           padding: EdgeInsets.symmetric(vertical: 23),
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(30)),
+                          primary: Color(0xFF3F7BFF),
                           elevation: 0,
                         )),
                   ),
